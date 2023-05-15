e994ab08eaf081d74adf2ccd5449924a4cb2b53d
KOH MENG YIT <my@My-MacBook-Air.local>
2023-05-15 15:39:06 +0800

    update redis endpoint, to use docker redis

--- CgWebsocket/settings.py
+++ CgWebsocket/settings.py
@@ -126,7 +126,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CACHES = {
     'default': {
         'BACKEND': 'django_redis.cache.RedisCache',
-        'LOCATION': 'redis://127.0.0.1:6379/',
+        'LOCATION': 'redis://redis:6379/',
         'OPTIONS': {
             'CLIENT_CLASS': 'django_redis.client.DefaultClient',
         }
