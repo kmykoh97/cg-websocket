443b590b16a60a3dd967b61ea3414fc170ee02a1
KOH MENG YIT <my@My-MacBook-Air.local>
2023-06-12 17:13:36 +0800

    make sure connection break when no data

--- binance/views.py
+++ binance/views.py
@@ -13,11 +13,15 @@ def cache_stream_data_from_stream_buffer_ticker(websocket_api_manager):
             exit(0)
         oldest_stream_data_from_stream_buffer = websocket_api_manager.pop_stream_data_from_stream_buffer()
         if oldest_stream_data_from_stream_buffer is False:
-            time.sleep(0.01)
+            time.sleep(5)
+            if websocket_api_manager.pop_stream_data_from_stream_buffer() is False:
+                exit(0)
         else:
+            cache.set("checkpoint_ticker", datetime.now(), 60)
             json_array = json.loads(oldest_stream_data_from_stream_buffer)
             for item in json_array:
                 cache.set(f"ticker_{item['s']}", item, 30)
+            time.sleep(3)
 
 def cache_stream_data_from_stream_buffer_orderbook(websocket_api_manager, cache_key):
     while True:
@@ -25,33 +29,36 @@ def cache_stream_data_from_stream_buffer_orderbook(websocket_api_manager, cache_
             exit(0)
         oldest_stream_data_from_stream_buffer = websocket_api_manager.pop_stream_data_from_stream_buffer()
         if oldest_stream_data_from_stream_buffer is False:
-            time.sleep(0.01)
+            time.sleep(5)
+            if websocket_api_manager.pop_stream_data_from_stream_buffer() is False:
+                exit(0)
         else:
+            cache.set(f"checkpoint_{cache_key}", datetime.now(), 60)
             cache.set(cache_key, oldest_stream_data_from_stream_buffer, 30)
+            time.sleep(3)
 
 def launch_ws_thread_for_ticker():
     mini_ticker_manager = BinanceWebSocketApiManager()
     t = threading.Thread(target=cache_stream_data_from_stream_buffer_ticker, args=[mini_ticker_manager], daemon=True)
     t.start()
     miniTicker_arr_stream_id = mini_ticker_manager.create_stream("arr", "!miniTicker")
-    time.sleep(1)  # to make sure every refresh there is result
-
 
 def launch_ws_thread_for_orderbook(ticker_symbol):
     orderbook_manager = BinanceWebSocketApiManager()
     t = threading.Thread(target=cache_stream_data_from_stream_buffer_orderbook, args=[orderbook_manager, f"orderbook_{ticker_symbol}"], daemon=True)
     t.start()
     orderbook_stream_id = orderbook_manager.create_stream("depth20", [ticker_symbol])
-    time.sleep(1)  # to make sure every refresh there are result
 
 def index(request):
     return HttpResponse("Hello world. You're at the binance index. Try /mini_tickers_bulk OR /mini_ticker_single/BTCUSDT OR /orderbook/BTCUSDT")
 
 def checkpoint_ticker_is_new(symbol):
     determinant = cache.get(f"checkpoint_{symbol}", "404")
-    cache.set(f"checkpoint_{symbol}", datetime.now(), 60)
 
-    if determinant == "404": return True
+    if determinant == "404":
+        cache.set(f"checkpoint_{symbol}", datetime.now(), 60)
+        return True
+
     return False
 
 def mini_tickers_bulk(request):
@@ -90,7 +97,4 @@ def orderbook(request, ticker_symbol):
         launch_ws_thread_for_orderbook(ticker_symbol)
         cached_result = cache.get(f"orderbook_{ticker_symbol}")
 
-    return JsonResponse(json.loads(cached_result))
-
-
-
+    return JsonResponse(json.loads(cached_result), safe=False)
